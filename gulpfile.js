a0b49f0dabd87f06394e6be9eea8795c5cfd5772
akkobtsev <akkobtsev@maximum-avto.ru>
2022-02-20 17:19:16 +0300

    Добавление шрифтов Red Hat. Добавление отдельной папки тайлов для сборки проекта. Теперь папка dist является реальнйо частью проекта, файлов которой нет в приложении. Добавлен интерфейс управления потребностями. Элементы управления настройками, чекбоками, модальными окнами теперь абстрагированы от конкретного функционала и глубже интегрирована в Bootstrap. Добавлены очередные библиотеки из Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,30 @@
 /* пути к исходным файлам (src), к готовым файлам (build), а также к тем, за изменениями которых нужно наблюдать (watch) */
 var path = {
 	dist: {
-		js: 'assets/dist/js/',
 		css: 'assets/dist/css/',
         img: 'assets/dist/img/',
+        tiles: 'assets/dist/tiles/',
 		fonts: 'assets/dist/fonts/'
 	},
 	build: {
 		html: 'assets/build/',
-		js: 'assets/build/js/',
 		css: 'assets/build/css/',
 		img: 'assets/build/img/',
+        tiles: 'assets/build/tiles/',
 		fonts: 'assets/build/fonts/'
 	},
 	src: {
 		html: 'assets/src/*.html',
-		js: 'assets/src/js/main.js',
 		style: 'assets/src/style/main.scss',
 		img: 'assets/src/img/*.*',
+        tiles: 'assets/src/tiles/*.*',
 		fonts: 'assets/src/fonts/**/*.*'
 	},
 	watch: {
 		html: 'assets/src/**/*.html',
-		js: 'assets/src/js/**/*.*',
 		css: 'assets/src/style/*.*',
 		img: 'assets/src/img/**/*.*',
+        tiles: 'assets/src/tiles/',
 		fonts: 'assets/src/fonts/*.*'
 	},
     cleanBuild: './assets/build/*',
@@ -87,22 +87,6 @@ gulp.task('css:build', function () {
 		.pipe(webserver.reload({ stream: true })); // перезагрузим сервер
 });
 
-// сбор js
-gulp.task('js:build', function () {
-
-	return gulp.src(path.src.js) // получим файл main.js
-		.pipe(plumber()) // для отслеживания ошибок
-		.pipe(rigger()) // импортируем все указанные файлы в main.js
-		.pipe(gulp.dest(path.build.js))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(sourcemaps.init()) //инициализируем sourcemap
-		.pipe(uglify()) // минимизируем js
-		.pipe(sourcemaps.write('./')) //  записываем sourcemap
-		.pipe(gulp.dest(path.build.js)) // положим готовый файл
-		.pipe(gulp.dest(path.dist.js)) // положим готовый файл
-		.pipe(webserver.reload({ stream: true })); // перезагрузим сервер
-});
-
 // перенос шрифтов
 gulp.task('fonts:build', function () {
 	return gulp.src(path.src.fonts)
@@ -115,6 +99,13 @@ gulp.task('image:build', function () {
 	return gulp.src(path.src.img) // путь с исходниками картинок
 	.pipe(gulp.dest(path.build.img))
     .pipe(gulp.dest(path.dist.img));
+});
+
+// обработка тайлов
+gulp.task('tiles:build', function () {
+	return gulp.src(path.src.tiles) // путь с исходниками картинок
+	.pipe(gulp.dest(path.build.tiles))
+    .pipe(gulp.dest(path.dist.tiles));
 });
 
 gulp.task('clean:dist', function () {
@@ -137,8 +128,8 @@ gulp.task('build',
 			'clean:dist',
 			'html:build',
 			'css:build',
-			'js:build',
 			'fonts:build',
+			'tiles:build',
 			'image:build'
 		)
 	)
@@ -148,9 +139,9 @@ gulp.task('build',
 gulp.task('watch', function () {
 	gulp.watch(path.watch.html, gulp.series('html:build'));
 	gulp.watch(path.watch.css, gulp.series('css:build'));
-	gulp.watch(path.watch.js, gulp.series('js:build'));
 	gulp.watch(path.watch.img, gulp.series('image:build'));
 	gulp.watch(path.watch.fonts, gulp.series('fonts:build'));
+	gulp.watch(path.watch.tiles, gulp.series('tiles:build'));
 });
 
 // задача по умолчанию
